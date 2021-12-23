bfac2f5306ac3f6fd9a426c019d18899deac6299
janaSunrise <warriordefenderz@gmail.com>
2021-12-23 10:02:30 +0530

    Fix a big bug of Banner not updating.
    Fix the issue of song and the top tracks info not being updated inside the while loop.

--- app/__main__.py
+++ app/__main__.py
@@ -9,21 +9,22 @@ from .image.generate import generate_image
 from .twitter import update_twitter_banner
 from .utils import get_song_info
 
-# Get top tracks.
-top_tracks = spotify.top_tracks(limit=5)
-
-top_tracks = [track for track in top_tracks["items"]]
-
-# Get song info with switch to recently played if no song is playing.
-song, is_playing = get_song_info(spotify=spotify)
-
-# Get the status.
-status = random.choice(Config.STATUS_MAPPING[is_playing]) + ":"
-
 # Update banner every 5 minutes, until stopped.
 while True:
+    # Get top tracks.
+    top_tracks = spotify.top_tracks(limit=5)
+
+    top_tracks = [track for track in top_tracks["items"]]
+
+    # Get song info with switch to recently played if no song is playing.
+    song, is_playing = get_song_info(spotify=spotify)
+
+    # Get the status.
+    status = random.choice(Config.STATUS_MAPPING[is_playing]) + ":"
+
     # Generate the spotify banner image.
     generate_image(status, is_playing, song, top_tracks, Config.IMAGE_PATH)
+    logger.info("Generated image.")
 
     # Update the banner
     update_twitter_banner(twitter)
@@ -31,4 +32,4 @@ while True:
 
     # Sleep for 5 minutes.
     logger.info("Sleeping for 5 minutes.")
-    time.sleep(5 * 60)
+    time.sleep(2 * 60)
